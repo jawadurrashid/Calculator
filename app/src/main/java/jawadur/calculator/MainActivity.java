359a6abe94e849dbd918db82c970a664178da2b9
Jawadur Rashid <jawadur.rashid@gmail.com>
2017-03-12 23:55:19 -0400

    Updated code, added features to application

--- app/src/main/java/jawadur/calculator/MainActivity.java
+++ app/src/main/java/jawadur/calculator/MainActivity.java
@@ -14,15 +14,15 @@ public class MainActivity extends AppCompatActivity {
     // Initializing widgets
 
     private EditText result;
-    private EditText newNumber;
+    private EditText inputNumber;
     private TextView displayOperation;
 
     // Variables to hold operands and type of calculations
 
     private Double operand1 = null;          //Null to indicate that the variables do not have a value yet
-    private Double operand2 = null;
-    private String pendingOperation = "=";
-
+    private String pendingOperation = "=";    //Keep scope of variables as narrow as possible
+    private static final String STATE_PENDING_OPERATION = "PendingOperation";
+    private static final String STATE_OPERAND1 = "Operand1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         // Associating fields with reference to widgets with in layout through id
 
         result = (EditText) findViewById(R.id.result);
-        newNumber = (EditText) findViewById(R.id.newNumber);
+        inputNumber = (EditText) findViewById(R.id.inputNumber);
         displayOperation = (TextView) findViewById(operation);
 
         // Initializing and associating calculator buttons with reference to layout
@@ -55,15 +55,16 @@ public class MainActivity extends AppCompatActivity {
         Button buttonMinus = (Button) findViewById(R.id.buttonMinus);
         Button buttonPlus = (Button) findViewById(R.id.buttonPlus);
         Button buttonEquals = (Button) findViewById(R.id.buttonEquals);
+        Button buttonNegative = (Button) findViewById(R.id.buttonNeg);
 
         //Creating a specific onClickListener for numerical buttons (operands will have their own onClickListener)
 
         View.OnClickListener listener = new View.OnClickListener() {   //"listener" holds reference to a new OnClick instance
             @Override
-            //OnClick method will read caption of button and then append it to the newNumber Edit Text widget
+            //OnClick method will read caption of button and then append it to the inputNumber Edit Text widget
             public void onClick(View v) {                 //When button is tapped and the Android framework calls the onClick method, it passes in a reference to a button that was tapped
                 Button b = (Button) v;             //Any widget (view) that is tapped can be passed as a parameter of OnClick as an instance of its base class, view.
-                newNumber.append(b.getText().toString());  //Not all views can have texts, thus before the getText method can be called we must cast it to a widget that can use the getText method (Button)
+                inputNumber.append(b.getText().toString());  //Not all views can have texts, thus before the getText method can be called we must cast it to a widget that can use the getText method (Button)
             }
         };
 
@@ -77,6 +78,9 @@ public class MainActivity extends AppCompatActivity {
             buttonList[i].setOnClickListener(listener);
         }*/
 
+
+        //Creating onClickListener for operands
+
         button0.setOnClickListener(listener);
         button1.setOnClickListener(listener);
         button2.setOnClickListener(listener);
@@ -89,18 +93,27 @@ public class MainActivity extends AppCompatActivity {
         button9.setOnClickListener(listener);
         buttonDot.setOnClickListener(listener);
 
-        //Creating onClickListener for operands
+
+        //Functionality of calculator; The arithmetic keys will cause previous operations to be performed and then become the next operation to be performed, and this is done by storing the operation with in the pending operation
 
         View.OnClickListener opListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Button b = (Button) v;   //Casts view to a button
                 String op = b.getText().toString(); //Obtains text read from button clicked and converts it into a string (stored in String op)
-                String value = newNumber.getText().toString(); // Reads number from the Edit Text portion and stores it into the String variable called value
+                String value = inputNumber.getText().toString(); // Reads number from the Edit Text portion and stores it into the String variable called value
 
-                if (value.length() != 0) {      //There may be a scenario where user clicks on operation button before a number, therefore we must make a condition for when the length of the value string is not zero
-                    performOperation(value, op);
+                try {
+                    Double doubleValue = Double.valueOf(value);
+                    performOperation(doubleValue, op);    // Even if there is an exception, code still sets pending operation otherwise the user will not be able to change the type of operation without entering some number and performing the current operation
+                } catch (NumberFormatException e) {    //Error found when inputting a single decimal point as input
+                    inputNumber.setText("");             // Code to be excecuted it there is an exception
                 }
+
+
+                /*if (value.length() != 0) {      //There may be a scenario where user clicks on operation button before a number, therefore we must make a condition for when the length of the value string is not zero
+                    performOperation(value, op);
+                }*/
 
                 pendingOperation = op;                //Storing text from "op" into a variable
                 displayOperation.setText(pendingOperation); //Setting the text into the "operations" widgets for display
@@ -121,49 +134,89 @@ public class MainActivity extends AppCompatActivity {
         buttonPlus.setOnClickListener(opListener);
         buttonDivide.setOnClickListener(opListener);
 
-
-    }
-
-    private void performOperation(String value, String operation) {
-        if (null == operand1) {    // The valueOf method returns the relevant Number Object holding the value of the argument passed.
-            operand1 = Double.valueOf(value);
-        } else {
-            operand2 = Double.valueOf(value);
-
-        if (pendingOperation.equals("=")) {
-            pendingOperation = operation;
-        }
-
-        switch (pendingOperation) {
-
-            case "=":
-                operand1 = operand2;
-                break;
-
-            case "/":
-                if (operand2 == 0) {
-                    operand1 = 0.0;
-                } else {
-                    operand1 /= operand2;
+        buttonNegative.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String value = inputNumber.getText().toString();
+                if (value.length() == 0) {          //If text in widget is empty, set to "-"
+                    inputNumber.setText("-");
+                } else {                             //Negate any number that is typed into widget, polarity updated by multiply by -1
+                    try {
+                        Double doubleValue = Double.valueOf(value);
+                        doubleValue *= -1;
+                        inputNumber.setText(doubleValue.toString()); //Widgets are updated into new number
+                    } catch (NumberFormatException e) {        //inputNumber is "-" or "." so clear it
+                        inputNumber.setText("");           // Invalid input cought by catch block; clear widget
+                    }
                 }
-                break;
 
-            case "*":
-                operand1 *= operand2;
-                break;
-
-            case "+":
-                operand1 += operand2;
-                break;
-
-            case "-":
-                operand1 -= operand2;
-                break;
-        }
+            }
+        });
     }
-    result.setText(operand1.toString());
-        newNumber.setText("");
 
- }
+    //When a number is inputted into the editText combined with an operation amd the orientation changes, the number will remain present during orientation change however operation will disappear
+    //onSaveInstanceState is called if needed when Android is about to destroy the activity, such as when orientation changes
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString(STATE_PENDING_OPERATION, pendingOperation); //Restoring variables by reading through bundle and also updating the display to see which operation is pending
+        if (operand1 != null) {      //Operand1 must not be null otherwise program will crash when orientation changes without any numbers inputted
+            outState.putDouble(STATE_OPERAND1, operand1);
+        }
+        super.onSaveInstanceState(outState);      //Saves this state for user
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        pendingOperation = savedInstanceState.getString(STATE_PENDING_OPERATION);
+        operand1 = savedInstanceState.getDouble(STATE_OPERAND1);
+        displayOperation.setText(pendingOperation);
+    }
+
+    private void performOperation(Double value, String operation) {
+
+        if (null == operand1) {    // The valueOf method returns the relevant Number Object holding the value of the argument passed.
+            operand1 = value;   //Lone decimal point input crashes app because string is unable to be converted to a double; must change data type of first parameter
+        } else {
+            if (pendingOperation.equals("=")) {
+                pendingOperation = operation;
+            }
+
+            switch (pendingOperation) {
+
+                case "=":
+                    operand1 = value;
+                    break;
+
+                case "/":
+                    if (value == 0) {
+                        operand1 = 0.0;
+                    } else {
+                        operand1 /= value;
+                    }
+                    break;
+
+                case "*":
+                    operand1 *= value;
+                    break;
+
+                case "+":
+                    operand1 += value;
+                    break;
+
+                case "-":
+                    operand1 -= value;
+                    break;
+            }
+        }
+        result.setText(operand1.toString());
+        inputNumber.setText("");
+
+
+    }
+
 }
+
+
 
